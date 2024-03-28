3096ec8af8a2129cb91f67054677ec04d5bcd7e3
TanyaTymoshchenko <ttymoshchenko@yahoo.com>
2024-03-29 00:12:21 +0200

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -14,7 +14,7 @@ export async function fetchPhotoFromPixabay() {
         inputValueForForm = input.trim().split(' ').join('+');
     }
     const searchParams = new URLSearchParams({
-        key: "42633759-4a44573e34755b4488adb4c1b",
+        key: "42956333-dca595600c3a5bd958b67132a",
         q: [inputValueForForm],
         image_type: "photo",
         orientation: "horizontal",
